34626c87586fe115181042069b6c7186544c1309
Lubomir Rintel <lkundrak@v3.sk>
2015-07-24 19:07:37 +0200

    auth-dialog: port to libnm

--- auth-dialog/main.c
+++ auth-dialog/main.c
@@ -33,10 +33,9 @@
 #define SECRET_API_SUBJECT_TO_CHANGE
 #include <libsecret/secret.h>
 
-#include <nm-setting-vpn.h>
-#include <nm-setting-connection.h>
-#include <nm-vpn-plugin-utils.h>
-#include <nm-vpn-password-dialog.h>
+#include <NetworkManager.h>
+#include <nm-vpn-service-plugin.h>
+#include <nma-vpn-password-dialog.h>
 
 #include <../src/nm-openswan-service.h>
 
@@ -319,7 +318,7 @@ get_pw_flags (GHashTable *hash, const char *secret_name, const char *mode_name)
 	NMSettingSecretFlags flags = NM_SETTING_SECRET_FLAG_NONE;
 
 	/* Try new flags value first */
-	if (nm_vpn_plugin_utils_get_secret_flags (hash, secret_name, &flags))
+	if (nm_vpn_service_plugin_get_secret_flags (hash, secret_name, &flags))
 		return flags;
 
 	/* Otherwise try old "password type" value */
@@ -473,7 +472,7 @@ main (int argc, char *argv[])
 		return 1;
 	}
 
-	if (!nm_vpn_plugin_utils_read_vpn_details (0, &data, &secrets)) {
+	if (!nm_vpn_service_plugin_read_vpn_details (0, &data, &secrets)) {
 		fprintf (stderr, "Failed to read '%s' (%s) data and secrets from stdin.\n",
 		         vpn_name, vpn_uuid);
 		return 1;
