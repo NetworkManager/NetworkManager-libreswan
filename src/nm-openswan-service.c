NetworkManager/NetworkManager-libreswan

commit 0cc023995ea3241ba1f36db4d876e9eb29cfb8d2

service: update the plugin state on failures
Otherwise the daemon does not notice we failed and times out.

src/nm-openswan-service.c
@@ -442,6 +442,7 @@ connect_failed (NMOpenSwanPlugin *self,
 	if (do_stop)
 		ipsec_stop (self, NULL);
 	nm_vpn_plugin_failure (NM_VPN_PLUGIN (self), reason);
+	nm_vpn_service_plugin_set_state (NM_VPN_SERVICE_PLUGIN (self), NM_VPN_SERVICE_STATE_STOPPED);
 }
 
 static gboolean

commit 7e897886ccbb63effaaea500bdfc5b4969e3293d

service: revert libnm-specific bits
Not there yet.

Fixes: 0cc023995ea3241ba1f36db4d876e9eb29cfb8d2

src/nm-openswan-service.c
@@ -442,7 +442,7 @@ connect_failed (NMOpenSwanPlugin *self,
 	if (do_stop)
 		ipsec_stop (self, NULL);
 	nm_vpn_plugin_failure (NM_VPN_PLUGIN (self), reason);
-	nm_vpn_service_plugin_set_state (NM_VPN_SERVICE_PLUGIN (self), NM_VPN_SERVICE_STATE_STOPPED);
+	nm_vpn_plugin_set_state (NM_VPN_PLUGIN (self), NM_VPN_SERVICE_STATE_STOPPED);
 }
 
 static gboolean