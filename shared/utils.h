94bc845704bee481ad6c9379b1a674489e306c36
Francesco Giudici <fgiudici@redhat.com>
2018-09-03 17:46:23 +0200

    all: add "_KEY" to the prefix of the libreswan key defines
    https://gitlab.gnome.org/GNOME/NetworkManager-libreswan/issues/4
    https://gitlab.gnome.org/GNOME/NetworkManager-libreswan/merge_requests/6

--- shared/utils.h
+++ shared/utils.h
@@ -50,7 +50,7 @@ nm_libreswan_utils_setting_is_ikev2 (NMSettingVpn *s_vpn, const char **out_ikev2
 {
 	const char *ikev2;
 
-	ikev2 = nm_setting_vpn_get_data_item (s_vpn, NM_LIBRESWAN_IKEV2);
+	ikev2 = nm_setting_vpn_get_data_item (s_vpn, NM_LIBRESWAN_KEY_IKEV2);
 	if (ikev2 && strlen (ikev2) && out_ikev2)
 		*out_ikev2 = ikev2;
 
