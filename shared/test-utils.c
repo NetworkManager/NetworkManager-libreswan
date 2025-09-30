NetworkManager/NetworkManager-libreswan

commit f3c6f38f3be353c5371c33abc971a451c1d24922

shared: make ipsec.conf formatting declarative
At this point it is obvious that being clever about adding properties
that the user didn't specify was not the greatest idea.
nm_libreswan_get_ipsec_conf() has became a sad and miserable mess.
This attempts to untangle it.

The idea is to have a declarative description of the properties,
along with a routine that generates them if unspecified and flags that
specify how is it formatted.

This hopefully breaks the logic down into managable pieces while keeping
nm_libreswan_get_ipsec_conf() reasonably simple. The data, along with
the routine that produces a full (canonical, sanitized) configuration is
kept separate, because it could be used on import as well (a follow-up
commit), chopping of annother bit that has grown too complicated.

Moreover it does validation as well, so separate nm_ls_*_validate()
could be dropped off. Yes, ha ha ha, *yes*!

https://gitlab.gnome.org/GNOME/NetworkManager-libreswan/-/merge_requests/38

shared/test-utils.c
@@ -40,19 +40,45 @@ test_config_write (void)
 	                 "conn con_name\n"
 	                 " ikev2=never\n"
 	                 " right=11.12.13.14\n"
-	                 " authby=secret\n"
 	                 " left=%defaultroute\n"
 	                 " leftmodecfgclient=yes\n"
-	                 " rightsubnet=0.0.0.0/0\n"
-	                 " leftxauthclient=yes\n"
-	                 " remote-peer-type=cisco\n"
-	                 " rightxauthserver=yes\n"
+	                 " authby=secret\n"
 	                 " ikelifetime=24h\n"
 	                 " salifetime=24h\n"
-	                 " keyingtries=1\n"
+	                 " rightsubnet=0.0.0.0/0\n"
 	                 " rekey=yes\n"
+	                 " keyingtries=1\n"
+	                 " leftxauthclient=yes\n"
+	                 " rightxauthserver=yes\n"
+	                 " remote-peer-type=cisco\n"
 	                 " rightmodecfgserver=yes\n"
 	                 " modecfgpull=yes\n");
+	g_free (str);
+	g_object_unref (s_vpn);
+
+	s_vpn = NM_SETTING_VPN (nm_setting_vpn_new ());
+	nm_setting_vpn_add_data_item (s_vpn, "right", "11.12.13.14");
+	nm_setting_vpn_add_data_item (s_vpn, "dhgroup", "ignored");
+	str = nm_libreswan_get_ipsec_conf (4, s_vpn, "con_name", NULL, FALSE, TRUE, &error);
+	g_assert_no_error (error);
+	g_assert_cmpstr (str, ==,
+	                 "conn con_name\n"
+	                 " ikev2=never\n"
+	                 " right=11.12.13.14\n"
+	                 " left=%defaultroute\n"
+	                 " leftmodecfgclient=yes\n"
+	                 " authby=secret\n"
+	                 " ikelifetime=24h\n"
+	                 " salifetime=24h\n"
+	                 " rightsubnet=0.0.0.0/0\n"
+	                 " rekey=yes\n"
+	                 " keyingtries=1\n"
+	                 " leftxauthclient=yes\n"
+	                 " rightxauthserver=yes\n"
+	                 " remote-peer-type=cisco\n"
+	                 " rightmodecfgserver=yes\n"
+	                 " modecfgpull=yes\n");
+
 	g_free (str);
 	g_object_unref (s_vpn);
 
@@ -71,13 +97,14 @@ test_config_write (void)
 	                 " right=11.12.13.14\n"
 	                 " leftid=%fromcert\n"
 	                 " leftcert=\"LibreswanClient\"\n"
-	                 " leftrsasigkey=\"%cert\"\n"
 	                 " rightrsasigkey=\"%cert\"\n"
+	                 " leftrsasigkey=\"%cert\"\n"
 	                 " left=%defaultroute\n"
 	                 " leftmodecfgclient=yes\n"
 	                 " rightsubnet=0.0.0.0/0\n"
-	                 " keyingtries=1\n"
 	                 " rekey=yes\n"
+	                 " phase2alg=aes256-sha1\n"
+	                 " keyingtries=1\n"
 	                 " rightmodecfgserver=yes\n"
 	                 " modecfgpull=yes\n");
 	g_free (str);
@@ -94,13 +121,14 @@ test_config_write (void)
 	                 "conn conn\n"
 	                 " ikev2=insist\n"
 	                 " right=11.12.13.14\n"
-	                 " leftrsasigkey=\"hello\"\n"
 	                 " rightrsasigkey=\"world\"\n"
+	                 " leftrsasigkey=\"hello\"\n"
 	                 " left=%defaultroute\n"
 	                 " leftmodecfgclient=yes\n"
 	                 " rightsubnet=0.0.0.0/0\n"
-	                 " keyingtries=1\n"
 	                 " rekey=yes\n"
+	                 " phase2alg=aes256-sha1\n"
+	                 " keyingtries=1\n"
 	                 " rightmodecfgserver=yes\n"
 	                 " modecfgpull=yes\n");
 	g_free (str);
@@ -115,25 +143,26 @@ test_config_write (void)
 	g_assert_no_error (error);
 	g_assert_cmpstr (str, ==,
 	                 "conn my_con\n"
-	                 " auto=add\n"
-	                 " nm-configured=yes\n"
-	                 " leftupdown=\"/foo/bar/ifupdown hello 123 456\"\n"
 	                 " ikev2=never\n"
 	                 " right=11.12.13.14\n"
-	                 " authby=secret\n"
 	                 " left=%defaultroute\n"
 	                 " leftmodecfgclient=yes\n"
-	                 " rightsubnet=0.0.0.0/0\n"
-	                 " leftxauthclient=yes\n"
-	                 " remote_peer_type=cisco\n"
-	                 " rightxauthserver=yes\n"
+	                 " authby=secret\n"
 	                 " ikelifetime=24h\n"
 	                 " salifetime=24h\n"
-	                 " keyingtries=1\n"
+	                 " rightsubnet=0.0.0.0/0\n"
 	                 " rekey=yes\n"
+	                 " keyingtries=1\n"
+	                 " leftxauthclient=yes\n"
+	                 " rightxauthserver=yes\n"
+	                 " remote_peer_type=cisco\n"
 	                 " rightmodecfgserver=yes\n"
-	                 " modecfgpull=yes");
+	                 " modecfgpull=yes\n"
+	                 " leftupdown=\"/foo/bar/ifupdown hello 123 456\"\n"
+	                 " auto=add\n"
+	                 " nm-configured=yes");
 	g_free (str);
+	g_object_unref (s_vpn);
 
 	s_vpn = NM_SETTING_VPN (nm_setting_vpn_new ());
 	str = nm_libreswan_get_ipsec_conf (4, s_vpn, "conn", NULL, FALSE, TRUE, &error);

configure.ac
@@ -145,9 +145,9 @@ if test x"$with_gtk4" != xno; then
 	PKG_CHECK_MODULES(LIBNMA_GTK4, libnma-gtk4 >= 1.8.33)
 fi
 
-PKG_CHECK_MODULES(LIBNM, libnm >= 1.2.0)
-LIBNM_CFLAGS="$LIBNM_CFLAGS -DNM_VERSION_MIN_REQUIRED=NM_VERSION_1_2"
-LIBNM_CFLAGS="$LIBNM_CFLAGS -DNM_VERSION_MAX_ALLOWED=NM_VERSION_1_2"
+PKG_CHECK_MODULES(LIBNM, libnm >= 1.12.0)
+LIBNM_CFLAGS="$LIBNM_CFLAGS -DNM_VERSION_MIN_REQUIRED=NM_VERSION_1_12"
+LIBNM_CFLAGS="$LIBNM_CFLAGS -DNM_VERSION_MAX_ALLOWED=NM_VERSION_1_12"
 
 NM_VPN_SERVICE_DIR=`$PKG_CONFIG --define-variable prefix='\${prefix}' --variable vpnservicedir libnm`
 AC_SUBST(NM_VPN_SERVICE_DIR)

shared/utils.h
@@ -18,7 +18,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Copyright (C) 2010 - 2015 Red Hat, Inc.
+ * Copyright (C) 2010 - 2024 Red Hat, Inc.
  */
 
 #ifndef __UTILS_H__
@@ -33,14 +33,11 @@ char *nm_libreswan_get_ipsec_conf (int ipsec_version,
                                    GError **error);
 
 static inline gboolean
-nm_libreswan_utils_setting_is_ikev2 (NMSettingVpn *s_vpn, const char **out_ikev2)
+nm_libreswan_utils_setting_is_ikev2 (NMSettingVpn *s_vpn)
 {
 	const char *ikev2;
 
 	ikev2 = nm_setting_vpn_get_data_item (s_vpn, NM_LIBRESWAN_KEY_IKEV2);
-	if (ikev2 && strlen (ikev2) && out_ikev2)
-		*out_ikev2 = ikev2;
-
 	return NM_IN_STRSET (ikev2,
 	                     NM_LIBRESWAN_IKEV2_PROPOSE,
 	                     NM_LIBRESWAN_IKEV2_YES,

src/nm-libreswan-service.c
@@ -1996,7 +1996,7 @@ _connect_common (NMVpnServicePlugin   *plugin,
 		return FALSE;
 
 	/* XAUTH is not part of the IKEv2 standard and we always enforce it in IKEv1 */
-	priv->xauth_enabled = !nm_libreswan_utils_setting_is_ikev2 (s_vpn, NULL);
+	priv->xauth_enabled = !nm_libreswan_utils_setting_is_ikev2 (s_vpn);
 
 	if (priv->xauth_enabled)
 		priv->password = g_strdup (nm_setting_vpn_get_secret (s_vpn, NM_LIBRESWAN_KEY_XAUTH_PASSWORD));
@@ -2082,7 +2082,7 @@ real_need_secrets (NMVpnServicePlugin *plugin,
 	}
 
 xauth_check:
-	if (!nm_libreswan_utils_setting_is_ikev2 (s_vpn, NULL)) {
+	if (!nm_libreswan_utils_setting_is_ikev2 (s_vpn)) {
 		pw_type = nm_setting_vpn_get_data_item (s_vpn, NM_LIBRESWAN_KEY_XAUTH_PASSWORD_INPUT_MODES);
 		if (!pw_type || strcmp (pw_type, NM_LIBRESWAN_PW_TYPE_UNUSED)) {
 			if (!nm_setting_vpn_get_secret (s_vpn, NM_LIBRESWAN_KEY_XAUTH_PASSWORD)) {

commit a0a93beb4f74cc67c8f123eee8aef503329b9238

utils: set "esp" when importing "phase2alg"
Filip reports [1]:

  1) esp= value is exported as phase2alg= , but phase2alg is ignored
  during import (if manually replaced in the exported file, esp= value
  is imported fine)

This fixes it by setting "esp" on import. Adds a test case.

[1] https://issues.redhat.com/browse/NMT-1746

Fixes: f3c6f38f3be3 ('shared: make ipsec.conf formatting declarative')

shared/utils.c
@@ -227,6 +227,8 @@ add_phase2alg (NMSettingVpn *s_vpn, const char *key, const char *val)
 
 	if (val == NULL || val[0] == '\0')
 		val = nm_setting_vpn_get_data_item (s_vpn, NM_LIBRESWAN_KEY_ESP);
+	else
+		nm_setting_vpn_add_data_item (s_vpn, NM_LIBRESWAN_KEY_ESP, val);
 	if (val == NULL || val[0] == '\0') {
 		leftid = nm_setting_vpn_get_data_item (s_vpn, NM_LIBRESWAN_KEY_LEFTID);
 		if (!nm_libreswan_utils_setting_is_ikev2 (s_vpn) && leftid && leftid[0] != '\0')
@@ -324,11 +326,11 @@ static const struct LibreswanParam params[] = {
 	/* Special. */
 	{ NM_LIBRESWAN_KEY_REKEY,                      add_rekey,             PARAM_PRINTABLE },
 	{ NM_LIBRESWAN_KEY_ESP,                        add                    },
-	{ "phase2alg",                                 add_phase2alg,         PARAM_PRINTABLE | PARAM_SYNTHETIC },
 	{ NM_LIBRESWAN_KEY_VENDOR,                     add                    },
 	{ "cisco-unity",                               add_cisco_unity,       PARAM_PRINTABLE | PARAM_SYNTHETIC },
 
 	/* Synthetic, not stored. */
+	{ "phase2alg",                                 add_phase2alg,         PARAM_PRINTABLE | PARAM_SYNTHETIC },
 	{ "keyingtries",                               add_keyingtries,       PARAM_PRINTABLE | PARAM_SYNTHETIC },
 	{ "aggrmode",                                  add_aggrmode,          PARAM_PRINTABLE | PARAM_SYNTHETIC },
 	{ "leftxauthclient",                           add_ikev1_yes,         PARAM_PRINTABLE | PARAM_SYNTHETIC },

shared/test-utils.c
@@ -184,6 +184,32 @@ test_config_write (void)
 	g_object_unref (s_vpn);
 
 	s_vpn = NM_SETTING_VPN (nm_setting_vpn_new ());
+	nm_setting_vpn_add_data_item (s_vpn, "right", "11.12.13.14");
+	nm_setting_vpn_add_data_item (s_vpn, "esp", "aes_gcm256");
+	str = nm_libreswan_get_ipsec_conf (4, s_vpn, "con_name", NULL, FALSE, TRUE, &error);
+	g_assert_no_error (error);
+	g_assert_cmpstr (str, ==,
+	                 "conn con_name\n"
+	                 " ikev2=never\n"
+	                 " right=11.12.13.14\n"
+	                 " left=%defaultroute\n"
+	                 " leftmodecfgclient=yes\n"
+	                 " authby=secret\n"
+	                 " ikelifetime=24h\n"
+	                 " salifetime=24h\n"
+	                 " rightsubnet=0.0.0.0/0\n"
+	                 " rekey=yes\n"
+	                 " phase2alg=aes_gcm256\n"
+	                 " keyingtries=1\n"
+	                 " leftxauthclient=yes\n"
+	                 " rightxauthserver=yes\n"
+	                 " remote-peer-type=cisco\n"
+	                 " rightmodecfgserver=yes\n"
+	                 " modecfgpull=yes\n");
+	g_free (str);
+	g_object_unref (s_vpn);
+
+	s_vpn = NM_SETTING_VPN (nm_setting_vpn_new ());
 	str = nm_libreswan_get_ipsec_conf (4, s_vpn, "conn", NULL, FALSE, TRUE, &error);
 	g_assert_error (error, NM_UTILS_ERROR, NM_UTILS_ERROR_INVALID_ARGUMENT);
 	g_assert_null (str);
@@ -510,6 +536,28 @@ test_config_read (void)
 	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "rightsubnet"),		==, "0.0.0.0/0");
 	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "salifetime"),		==, "24h");
 	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "vendor"),		==, "Cisco");
+	g_object_unref (s_vpn);
+	g_clear_pointer (&con_name, g_free);
+
+	/* Synchronize esp and phase2alg. */
+	s_vpn = nm_libreswan_parse_ipsec_conf (
+		"conn conn\n"
+		" right=11.12.13.14\n"
+		" phase2alg=aes128-sha2_512;dh19\n",
+		&con_name,
+		&error);
+	g_assert_no_error (error);
+	g_assert_cmpint (nm_setting_vpn_get_num_data_items (s_vpn), ==, 10);
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "authby"),		==, "secret");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "ikelifetime"),		==, "24h");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "ikev2"),			==, "never");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "left"),			==, "%defaultroute");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "leftmodecfgclient"),	==, "yes");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "rekey"),			==, "yes");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "right"),			==, "11.12.13.14");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "rightsubnet"),		==, "0.0.0.0/0");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "salifetime"),		==, "24h");
+	g_assert_cmpstr (nm_setting_vpn_get_data_item (s_vpn, "esp"),			==, "aes128-sha2_512;dh19");
 	g_object_unref (s_vpn);
 	g_clear_pointer (&con_name, g_free);
 