NetworkManager/NetworkManager-libreswan

commit 80bef36091e40ef86a0ff2215b4067ee81243415

Support `rightca` option
Supporting ipsec `rightca` option for user to define CA of remote host.

Unit test case included.

Resolves: https://issues.redhat.com/browse/RHEL-114701

Signed-off-by: Gris Ge <fge@redhat.com>

properties/nm-libreswan-editor.c
@@ -459,6 +459,7 @@ populate_adv_dialog (LibreswanEditor *self)
 	populate_adv (self, "remote_cert_entry", NM_LIBRESWAN_KEY_RIGHTCERT, NULL, NULL);
 	populate_adv (self, "require_id_on_certificate_checkbutton", NM_LIBRESWAN_KEY_REQUIRE_ID_ON_CERTIFICATE, NULL, "no");
 	populate_adv (self, "leftsendcert_entry", NM_LIBRESWAN_KEY_LEFTSENDCERT, NULL, NULL);
+	populate_adv (self, "rightca", NM_LIBRESWAN_KEY_RIGHTCA, NULL, NULL);
 	adv_changed_cb (NULL, self);
 }
 
@@ -774,6 +775,14 @@ update_adv_settings (LibreswanEditor *self, NMSettingVpn *s_vpn)
 		nm_setting_vpn_add_data_item (s_vpn, NM_LIBRESWAN_KEY_LEFTSENDCERT, str);
 	else
 		nm_setting_vpn_remove_data_item (s_vpn, NM_LIBRESWAN_KEY_LEFTSENDCERT);
+
+	/* rightca */
+	widget = GTK_WIDGET (gtk_builder_get_object (priv->builder, "rightca_entry"));
+	str = gtk_editable_get_text (GTK_EDITABLE (widget));
+	if (str && *str)
+		nm_setting_vpn_add_data_item (s_vpn, NM_LIBRESWAN_KEY_RIGHTCA, str);
+	else
+		nm_setting_vpn_remove_data_item (s_vpn, NM_LIBRESWAN_KEY_RIGHTCA);
 
 }
 

man/nm-settings-libreswan.5.in
@@ -192,6 +192,10 @@ This option configures when Libreswan will send X.509 certificates to the
 remote host. Example values are: 'yes', 'always', 'no', 'never', 'sendifasked'.
 NetworkManager-libreswan does not validate this option but merely pass to
 Libreswan daemon. Please check Libreswan document for detail.
+.TP
+.I "rightca"
+This option specifies the authorized Certificate Authority (CA) that signed
+the certificate of the remote host.
 
 .SH VPN.SECRETS
 .PP

properties/nm-libreswan-dialog.ui
@@ -1275,6 +1275,33 @@ config: authby &lt;value&gt;
                         <property name="top_attach">2</property>
                       </packing>
                     </child>
+                    <child>
+                      <object class="GtkLabel" id="rightca_label">
+                        <property name="visible">True</property>
+                        <property name="can_focus">False</property>
+                        <property name="label" translatable="yes">Send certificate(rightca)</property>
+                        <property name="use_underline">True</property>
+                        <property name="mnemonic_widget">rightca_entry</property>
+                        <property name="xalign">1</property>
+                      </object>
+                      <packing>
+                        <property name="left_attach">0</property>
+                        <property name="top_attach">3</property>
+                      </packing>
+                    </child>
+                    <child>
+                      <object class="GtkEntry" id="rightca_entry">
+                        <property name="visible">True</property>
+                        <property name="can_focus">True</property>
+                        <property name="tooltip_text" translatable="yes">Certificate Authority (CA) that signed the certificate of the remote host.</property>
+                        <property name="invisible_char">●</property>
+                        <property name="hexpand">True</property>
+                      </object>
+                      <packing>
+                        <property name="left_attach">1</property>
+                        <property name="top_attach">3</property>
+                      </packing>
+                    </child>
                   </object>
                 </child>
               </object>

shared/nm-service-defines.h
@@ -78,6 +78,7 @@
 #define NM_LIBRESWAN_KEY_REQUIRE_ID_ON_CERTIFICATE  "require-id-on-certificate"
 #define NM_LIBRESWAN_KEY_NM_AUTO_DEFAULTS           "nm-auto-defaults"
 #define NM_LIBRESWAN_KEY_LEFTSENDCERT               "leftsendcert"
+#define NM_LIBRESWAN_KEY_RIGHTCA                    "rightca"
 
 #define NM_LIBRESWAN_IKEV2_NO      "no"
 #define NM_LIBRESWAN_IKEV2_NEVER   "never"

shared/test-utils.c
@@ -169,6 +169,7 @@ test_config_write (void)
 	nm_setting_vpn_add_data_item (s_vpn, "right", "11.12.13.14");
 	nm_setting_vpn_add_data_item (s_vpn, "nm-auto-defaults", "false");
 	nm_setting_vpn_add_data_item (s_vpn, "leftsendcert", "always");
+	nm_setting_vpn_add_data_item (s_vpn, "rightca", "%same");
 	str = nm_libreswan_get_ipsec_conf (4, s_vpn, "conn", NULL, FALSE, TRUE, &error);
 	g_assert_no_error (error);
 	g_assert_cmpstr (str, ==,
@@ -177,7 +178,8 @@ test_config_write (void)
                          " right=11.12.13.14\n"
                          " rightrsasigkey=\"world\"\n"
                          " leftrsasigkey=\"hello\"\n"
-                         " leftsendcert=always\n");
+                         " leftsendcert=always\n"
+                         " rightca=\"%same\"\n");
 	g_free (str);
 	g_object_unref (s_vpn);
 

shared/utils.c
@@ -319,6 +319,7 @@ static const struct LibreswanParam params[] = {
 	{ NM_LIBRESWAN_KEY_TYPE,                       add,                   PARAM_PRINTABLE },
 	{ NM_LIBRESWAN_KEY_REQUIRE_ID_ON_CERTIFICATE,  add,                   PARAM_PRINTABLE },
 	{ NM_LIBRESWAN_KEY_LEFTSENDCERT,               add,                   PARAM_PRINTABLE },
+	{ NM_LIBRESWAN_KEY_RIGHTCA,                    add,                   PARAM_STRING },
 
 	/* Special. */
 	{ NM_LIBRESWAN_KEY_REKEY,                      add_rekey,             PARAM_PRINTABLE },

commit 5ffd14686876fb7d30688bb858fb25f8e05b3cda

properties: fix rightca widget name
Fixes: 80bef36091e4 ('Support `rightca` option')

properties/nm-libreswan-editor.c
@@ -484,7 +484,7 @@ populate_adv_dialog(LibreswanEditor *self)
 	             NULL,
 	             "no");
 	populate_adv(self, "leftsendcert_entry", NM_LIBRESWAN_KEY_LEFTSENDCERT, NULL, NULL);
-	populate_adv(self, "rightca", NM_LIBRESWAN_KEY_RIGHTCA, NULL, NULL);
+	populate_adv(self, "rightca_entry", NM_LIBRESWAN_KEY_RIGHTCA, NULL, NULL);
 	adv_changed_cb(NULL, self);
 }
 